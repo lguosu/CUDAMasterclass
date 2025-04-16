86eaa4bc662be33574654da920193116babc4ab5
larryleguo <larryleguo@gmail.com>
2025-04-16 10:34:55 +0800

    Update Section 2 profile script

--- Section2/7_sum_array-profile.sh
+++ Section2/7_sum_array-profile.sh
@@ -3,11 +3,16 @@
 # Do not use -g or -G flag since it will disable the optimization
 # and generate a lot of debug information
 
-cd ..
-nvcc -c common.cpp -o common.obj
+cd ../common
+nvcc -c common.cpp -o common.o
 cd -
-nvcc -link ../common/common.obj -o profiling_test.out 7_sum_array.cu -I../common
+nvcc -link ../common/common.o -o profiling_test.exe 7_sum_array.cu -I../common
 
-./profiling_test.out
+./profiling_test.exe
 
-nvprof ./profiling_test.out
+# nvprof is not supported on devices with compute capability 8.0 and higher.
+# Use NVIDIA Nsight Systems for GPU tracing and CPU sampling and NVIDIA Nsight Compute for GPU profiling.
+# Refer https://developer.nvidia.com/tools-overview for more details.
+# For command line tool, use ncu command.
+ncu ./profiling_test.exe
+ncu --metrics gld_efficiency,sm_efficiency,achieved_occupancy profiling_test.exe 1 25 20 8 2
