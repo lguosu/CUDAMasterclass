de3daa2dbc8684650ccf4ade885ddc63771faca7
larryleguo <larryleguo@gmail.com>
2025-04-20 16:35:31 +0800

    Code refactoring and new build rules
    Refactor two programs that compare the performance of float and double
    with C++ templates.
    
    Add new rules to the Makefile to build the ptx files to show the
    difference between standard and intrinsic functions.

--- Section6/2_floating_point_performance_comparison.cu
+++ Section6/2_floating_point_performance_comparison.cu
@@ -6,238 +6,164 @@
 #include "device_launch_parameters.h"
 #include "cuda_common.cuh"
 
-__global__ void lots_of_float_compute(float *inputs, int N, size_t niters,
-	float *outputs)
+// Template kernel for both float and double computations
+template<typename T>
+__global__ void lots_of_compute(T *inputs, int N, size_t niters, T *outputs)
 {
-	size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
-	size_t nthreads = gridDim.x * blockDim.x;
+    size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t nthreads = gridDim.x * blockDim.x;
 
-	for (; tid < N; tid += nthreads)
-	{
-		size_t iter;
-		float val = inputs[tid];
+    for (; tid < N; tid += nthreads)
+    {
+        size_t iter;
+        T val = inputs[tid];
 
-		for (iter = 0; iter < niters; iter++)
-		{
-			val = (val + 5.0f) - 101.0f;
-			val = (val / 3.0f) + 102.0f;
-			val = (val + 1.07f) - 103.0f;
-			val = (val / 1.037f) + 104.0f;
-			val = (val + 3.00f) - 105.0f;
-			val = (val / 0.22f) + 106.0f;
-		}
+        for (iter = 0; iter < niters; iter++)
+        {
+            val = (val + T(5.0)) - T(101.0);
+            val = (val / T(3.0)) + T(102.0);
+            val = (val + T(1.07)) - T(103.0);
+            val = (val / T(1.037)) + T(104.0);
+            val = (val + T(3.00)) - T(105.0);
+            val = (val / T(0.22)) + T(106.0);
+        }
 
-		outputs[tid] = val;
-	}
+        outputs[tid] = val;
+    }
 }
 
-__global__ void lots_of_double_compute(double *inputs, int N, size_t niters,
-	double *outputs)
+// Template function for running tests
+template<typename T>
+static void run_test(size_t N, int niters, int blocksPerGrid, int threadsPerBlock,
+    long* to_device_clock_cyl, long* kernel_clock_cyl, long* from_device_clock_cyl,
+    T* sample, int sampleLength)
 {
-	size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
-	size_t nthreads = gridDim.x * blockDim.x;
+    int i;
+    T *h_inputs, *h_outputs;
+    T *d_inputs, *d_outputs;
 
-	for (; tid < N; tid += nthreads)
-	{
-		size_t iter;
-		double val = inputs[tid];
+    h_inputs = (T*)malloc(sizeof(T) * N);
+    h_outputs = (T*)malloc(sizeof(T) * N);
+    gpuErrchk(cudaMalloc((void**)&d_inputs, sizeof(T) * N));
+    gpuErrchk(cudaMalloc((void**)&d_outputs, sizeof(T) * N));
 
-		for (iter = 0; iter < niters; iter++)
-		{
-			val = (val + 5.0) - 101.0;
-			val = (val / 3.0) + 102.0;
-			val = (val + 1.07) - 103.0;
-			val = (val / 1.037) + 104.0;
-			val = (val + 3.00) - 105.0;
-			val = (val / 0.22) + 106.0;
-		}
+    for (i = 0; i < N; i++)
+    {
+        h_inputs[i] = (T)i;
+    }
 
-		outputs[tid] = val;
-	}
-}
+    clock_t ops_start, ops_end;
 
-static void run_float_test(size_t N, int niters, int blocksPerGrid,int threadsPerBlock,
-	long* to_device_clock_cyl,long * kernel_clock_cyl, long* from_device_clock_cyl,
-	float *sample, int sampleLength) 
-{
-	int i;
-	float *h_floatInputs, *h_floatOutputs;
-	float *d_floatInputs, *d_floatOutputs;
+    ops_start = clock();
+    gpuErrchk(cudaMemcpy(d_inputs, h_inputs, sizeof(T) * N, cudaMemcpyHostToDevice));
+    ops_end = clock();
+    *to_device_clock_cyl = ops_end - ops_start;
 
-	h_floatInputs = (float *)malloc(sizeof(float) * N);
-	h_floatOutputs = (float *)malloc(sizeof(float) * N);
-	gpuErrchk(cudaMalloc((void **)&d_floatInputs, sizeof(float) * N));
-	gpuErrchk(cudaMalloc((void **)&d_floatOutputs, sizeof(float) * N));
+    ops_start = clock();
+    lots_of_compute<T><<<blocksPerGrid, threadsPerBlock>>>(d_inputs, N, niters, d_outputs);
+    gpuErrchk(cudaDeviceSynchronize());
+    ops_end = clock();
+    *kernel_clock_cyl = ops_end - ops_start;
 
-	for (i = 0; i < N; i++)
-	{
-		h_floatInputs[i] = (float)i;
-	}
+    ops_start = clock();
+    gpuErrchk(cudaMemcpy(h_outputs, d_outputs, sizeof(T) * N, cudaMemcpyDeviceToHost));
+    ops_end = clock();
+    *from_device_clock_cyl = ops_end - ops_start;
 
-	clock_t ops_start, ops_end;
+    for (i = 0; i < sampleLength; i++)
+    {
+        sample[i] = h_outputs[i];
+    }
 
-	ops_start = clock();
-	gpuErrchk(cudaMemcpy(d_floatInputs, h_floatInputs, sizeof(float) * N,cudaMemcpyHostToDevice));
-	ops_end = clock();
-	*to_device_clock_cyl = ops_end - ops_start;
-
-	ops_start = clock();
-	lots_of_float_compute << <blocksPerGrid, threadsPerBlock >> >(d_floatInputs,N, niters, d_floatOutputs);
-	gpuErrchk(cudaDeviceSynchronize());
-	ops_end = clock();
-	*kernel_clock_cyl = ops_end - ops_start;
-
-
-	ops_start = clock();
-	gpuErrchk(cudaMemcpy(h_floatOutputs, d_floatOutputs, sizeof(float) * N,cudaMemcpyDeviceToHost));
-	ops_end = clock();
-	*from_device_clock_cyl = ops_end - ops_start;
-
-
-	for (i = 0; i < sampleLength; i++)
-	{
-		sample[i] = h_floatOutputs[i];
-	}
-
-	gpuErrchk(cudaFree(d_floatInputs));
-	gpuErrchk(cudaFree(d_floatOutputs));
-	free(h_floatInputs);
-	free(h_floatOutputs);
-}
-
-static void run_double_test(size_t N, int niters, int blocksPerGrid,int threadsPerBlock, 
-	long* to_device_clock_cyl,long * kernel_clock_cyl, long* from_device_clock_cyl,
-	double *sample, int sampleLength)
-{
-	int i;
-	double *h_doubleInputs, *h_doubleOutputs;
-	double *d_doubleInputs, *d_doubleOutputs;
-
-	h_doubleInputs = (double *)malloc(sizeof(double) * N);
-	h_doubleOutputs = (double *)malloc(sizeof(double) * N);
-	gpuErrchk(cudaMalloc((void **)&d_doubleInputs, sizeof(double) * N));
-	gpuErrchk(cudaMalloc((void **)&d_doubleOutputs, sizeof(double) * N));
-
-	for (i = 0; i < N; i++)
-	{
-		h_doubleInputs[i] = (double)i;
-	}
-
-	clock_t ops_start, ops_end;
-
-	ops_start = clock();
-	gpuErrchk(cudaMemcpy(d_doubleInputs, h_doubleInputs, sizeof(double) * N,
-		cudaMemcpyHostToDevice));
-	ops_end = clock();
-	*to_device_clock_cyl = ops_end - ops_start;
-
-	ops_start = clock();
-	lots_of_double_compute << <blocksPerGrid, threadsPerBlock >> >(d_doubleInputs,
-		N, niters, d_doubleOutputs);
-	gpuErrchk(cudaDeviceSynchronize());
-	ops_end = clock();
-	*kernel_clock_cyl = ops_end - ops_start;
-
-	ops_start = clock();
-	gpuErrchk(cudaMemcpy(h_doubleOutputs, d_doubleOutputs, sizeof(double) * N,
-		cudaMemcpyDeviceToHost));
-	ops_end = clock();
-	*from_device_clock_cyl = ops_end - ops_start;
-
-	for (i = 0; i < sampleLength; i++)
-	{
-		sample[i] = h_doubleOutputs[i];
-	}
-
-	gpuErrchk(cudaFree(d_doubleInputs));
-	gpuErrchk(cudaFree(d_doubleOutputs));
-	free(h_doubleInputs);
-	free(h_doubleOutputs);
+    gpuErrchk(cudaFree(d_inputs));
+    gpuErrchk(cudaFree(d_outputs));
+    free(h_inputs);
+    free(h_outputs);
 }
 
 int main(int argc, char **argv)
 {
-	int i;
-	double meanFloatToDeviceTime, meanFloatKernelTime, meanFloatFromDeviceTime;
-	double meanDoubleToDeviceTime, meanDoubleKernelTime,
-		meanDoubleFromDeviceTime;
-	struct cudaDeviceProp deviceProperties;
-	size_t totalMem, freeMem;
-	float *floatSample;
-	double *doubleSample;
-	int sampleLength = 10;
-	int nRuns = 5;
-	int nKernelIters = 20;
+    int i;
+    double meanFloatToDeviceTime, meanFloatKernelTime, meanFloatFromDeviceTime;
+    double meanDoubleToDeviceTime, meanDoubleKernelTime,
+        meanDoubleFromDeviceTime;
+    struct cudaDeviceProp deviceProperties;
+    size_t totalMem, freeMem;
+    float *floatSample;
+    double *doubleSample;
+    int sampleLength = 10;
+    int nRuns = 5;
+    int nKernelIters = 20;
 
-	meanFloatToDeviceTime = meanFloatKernelTime = meanFloatFromDeviceTime = 0.0;
-	meanDoubleToDeviceTime = meanDoubleKernelTime =
-		meanDoubleFromDeviceTime = 0.0;
+    meanFloatToDeviceTime = meanFloatKernelTime = meanFloatFromDeviceTime = 0.0;
+    meanDoubleToDeviceTime = meanDoubleKernelTime =
+        meanDoubleFromDeviceTime = 0.0;
 
-	gpuErrchk(cudaMemGetInfo(&freeMem, &totalMem));
-	gpuErrchk(cudaGetDeviceProperties(&deviceProperties, 0));
+    gpuErrchk(cudaMemGetInfo(&freeMem, &totalMem));
+    gpuErrchk(cudaGetDeviceProperties(&deviceProperties, 0));
 
-	size_t N = (freeMem * 0.9 / 2) / sizeof(double);
-	int threadsPerBlock = 256;
-	int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    size_t N = (freeMem * 0.9 / 2) / sizeof(double);
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-	if (blocksPerGrid > deviceProperties.maxGridSize[0])
-	{
-		blocksPerGrid = deviceProperties.maxGridSize[0];
-	}
+    if (blocksPerGrid > deviceProperties.maxGridSize[0])
+    {
+        blocksPerGrid = deviceProperties.maxGridSize[0];
+    }
 
-	printf("Running %d blocks with %d threads/block over %lu elements\n",
-		blocksPerGrid, threadsPerBlock, N);
+    printf("Running %d blocks with %d threads/block over %lu elements\n",
+        blocksPerGrid, threadsPerBlock, N);
 
-	floatSample = (float *)malloc(sizeof(float) * sampleLength);
-	doubleSample = (double *)malloc(sizeof(double) * sampleLength);
+    floatSample = (float *)malloc(sizeof(float) * sampleLength);
+    doubleSample = (double *)malloc(sizeof(double) * sampleLength);
 
-	for (i = 0; i < nRuns; i++)
-	{
-		long toDeviceTime, kernelTime, fromDeviceTime;
+    for (i = 0; i < nRuns; i++)
+    {
+        long toDeviceTime, kernelTime, fromDeviceTime;
 
-		run_float_test(N, nKernelIters, blocksPerGrid, threadsPerBlock,
-			&toDeviceTime, &kernelTime, &fromDeviceTime,
-			floatSample, sampleLength);
-		meanFloatToDeviceTime += toDeviceTime;
-		meanFloatKernelTime += kernelTime;
-		meanFloatFromDeviceTime += fromDeviceTime;
+        run_test<float>(N, nKernelIters, blocksPerGrid, threadsPerBlock,
+            &toDeviceTime, &kernelTime, &fromDeviceTime,
+            floatSample, sampleLength);
+        meanFloatToDeviceTime += toDeviceTime;
+        meanFloatKernelTime += kernelTime;
+        meanFloatFromDeviceTime += fromDeviceTime;
 
-		run_double_test(N, nKernelIters, blocksPerGrid, threadsPerBlock,
-			&toDeviceTime, &kernelTime, &fromDeviceTime,
-			doubleSample, sampleLength);
-		meanDoubleToDeviceTime += toDeviceTime;
-		meanDoubleKernelTime += kernelTime;
-		meanDoubleFromDeviceTime += fromDeviceTime;
-	}
+        run_test<double>(N, nKernelIters, blocksPerGrid, threadsPerBlock,
+            &toDeviceTime, &kernelTime, &fromDeviceTime,
+            doubleSample, sampleLength);
+        meanDoubleToDeviceTime += toDeviceTime;
+        meanDoubleKernelTime += kernelTime;
+        meanDoubleFromDeviceTime += fromDeviceTime;
+    }
 
-	meanFloatToDeviceTime /= nRuns;
-	meanFloatKernelTime /= nRuns;
-	meanFloatFromDeviceTime /= nRuns;
-	meanDoubleToDeviceTime /= nRuns;
-	meanDoubleKernelTime /= nRuns;
-	meanDoubleFromDeviceTime /= nRuns;
+    meanFloatToDeviceTime /= nRuns;
+    meanFloatKernelTime /= nRuns;
+    meanFloatFromDeviceTime /= nRuns;
+    meanDoubleToDeviceTime /= nRuns;
+    meanDoubleKernelTime /= nRuns;
+    meanDoubleFromDeviceTime /= nRuns;
 
-	meanFloatToDeviceTime /= CLOCKS_PER_SEC;
-	meanFloatKernelTime /= CLOCKS_PER_SEC;
-	meanFloatFromDeviceTime /= CLOCKS_PER_SEC;
-	meanDoubleToDeviceTime /= CLOCKS_PER_SEC;
-	meanDoubleKernelTime /= CLOCKS_PER_SEC;
-	meanDoubleFromDeviceTime /= CLOCKS_PER_SEC;
+    meanFloatToDeviceTime /= CLOCKS_PER_SEC;
+    meanFloatKernelTime /= CLOCKS_PER_SEC;
+    meanFloatFromDeviceTime /= CLOCKS_PER_SEC;
+    meanDoubleToDeviceTime /= CLOCKS_PER_SEC;
+    meanDoubleKernelTime /= CLOCKS_PER_SEC;
+    meanDoubleFromDeviceTime /= CLOCKS_PER_SEC;
 
-	printf("For single-precision floating point, mean times for:\n");
-	printf("  Copy to device:   %f s\n", meanFloatToDeviceTime);
-	printf("  Kernel execution: %f s\n", meanFloatKernelTime);
-	printf("  Copy from device: %f s\n", meanFloatFromDeviceTime);
-	printf("For double-precision floating point, mean times for:\n");
-	printf("  Copy to device:   %f s (%.2fx slower than single-precision)\n",
-		meanDoubleToDeviceTime,
-		meanDoubleToDeviceTime / meanFloatToDeviceTime);
-	printf("  Kernel execution: %f s (%.2fx slower than single-precision)\n",
-		meanDoubleKernelTime,
-		meanDoubleKernelTime / meanFloatKernelTime);
-	printf("  Copy from device: %f s (%.2fx slower than single-precision)\n",
-		meanDoubleFromDeviceTime,
-		meanDoubleFromDeviceTime / meanFloatFromDeviceTime);
+    printf("For single-precision floating point, mean times for:\n");
+    printf("  Copy to device:   %f s\n", meanFloatToDeviceTime);
+    printf("  Kernel execution: %f s\n", meanFloatKernelTime);
+    printf("  Copy from device: %f s\n", meanFloatFromDeviceTime);
+    printf("For double-precision floating point, mean times for:\n");
+    printf("  Copy to device:   %f s (%.2fx slower than single-precision)\n",
+        meanDoubleToDeviceTime,
+        meanDoubleToDeviceTime / meanFloatToDeviceTime);
+    printf("  Kernel execution: %f s (%.2fx slower than single-precision)\n",
+        meanDoubleKernelTime,
+        meanDoubleKernelTime / meanFloatKernelTime);
+    printf("  Copy from device: %f s (%.2fx slower than single-precision)\n",
+        meanDoubleFromDeviceTime,
+        meanDoubleFromDeviceTime / meanFloatFromDeviceTime);
 
-	return 0;
+    return 0;
 }
--- Section6/2_numeriacal_accuracy_float_vs_double.cu
+++ Section6/2_numeriacal_accuracy_float_vs_double.cu
@@ -3,40 +3,27 @@
 #include <math.h>
 #include <stdint.h>
 
-void float_accuracy_comparison()
+template<typename T>
+void accuracy_comparison(const char* type_name)
 {
-	printf("float accuracy comparison \n");
-	float a = 3.1415927f;
-	float b = 3.1415928f;
-	if (a == b)
-	{
-		printf("a is equal to b\n");
-	}
-	else
-	{
-		printf("a does not equal b\n");
-	}
-}
-
-void double_accuracy_comparison()
-{
-	printf("\ndouble accuracy comparison \n");
-	double a = 3.1415927;
-	double b = 3.1415928;
-	if (a == b)
-	{
-		printf("a is equal to b\n");
-	}
-	else
-	{
-		printf("a does not equal b\n");
-	}
+    printf("%s accuracy comparison\n", type_name);
+    T a = T(3.1415927);
+    T b = T(3.1415928);
+    
+    if (a == b)
+    {
+        printf("a is equal to b\n");
+    }
+    else
+    {
+        printf("a does not equal b\n");
+    }
 }
 
 int main()
 {
-	float_accuracy_comparison();
-	double_accuracy_comparison();
+    accuracy_comparison<float>("float");
+    accuracy_comparison<double>("double");
 
-	return 0;
+    return 0;
 }
